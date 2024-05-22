99ff9e76408c8619f3feed94fb48fd185f6e5683
Assil <astradhdbjd@gmail.com>
2024-05-22 15:54:33 +0100

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFFFFFFFF);
+const Color kSecondaryColor = Color(0xFFB3B3B3);
+const Color kBackgroundColor = Color(0xFF0B032D);
+const Color kstarsColor = Color(0xFFF9D342);
