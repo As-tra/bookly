16ac4b1ce105dd19832815c214cd401902402733
Assil <astradhdbjd@gmail.com>
2024-05-26 07:14:54 +0100

    change getX with go_router for the navigation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,7 @@
-import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
-import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/slidding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/get_core.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -60,11 +57,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 4)).then(
-      (value) => Get.to(
-        const HomeView(),
-        transition: Transition.fade,
-        duration: kTransitionDuration,
-      ),
+      (value) => GoRouter.of(context).push('/home'),
     );
   }
 }
