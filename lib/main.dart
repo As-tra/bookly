eee58f3bd8c8781d4688442fd39167bf53208648
Assil <astradhdbjd@gmail.com>
2024-05-31 21:26:16 +0100

    fix the featured books modal issue

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/presentation/manager/NewestBooks/newest_books_cubit.dart';
 import 'package:bookly/features/home/presentation/manager/featuredBooksCubit/featured_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
